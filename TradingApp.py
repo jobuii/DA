96ddb4e37bca4d7a099a5a279c6d9312433b32dd
Dusko <macros_85@hotmail.com>
2018-10-02 14:01:35 +0100

    initial commit

---
+++ TradingApp.py
@@ -0,0 +1,27 @@
+import json
+import requests
+import API
+x = int(1)
+while x < 600:
+    r = requests.get('https://cex.io/api/order_book/ETH/EUR/')
+    json_data = json.loads(r.text)
+    tradefee = 0.0016
+    topask = json_data['asks'][000][0]
+    topbid = json_data['bids'][000][0]
+    netProfit = (topask * (1 - tradefee)) - (topbid * (1 + tradefee))
+    if netProfit > 0:
+        print('Profit')
+    else:
+        print('Loss')
+    netFee = (topask + topbid) * tradefee
+    print(netFee)
+    x += 1
+
+
+#Account ID: up108093172
+#Key: 75Agih0SMZKysoskBVxJ9bcc0E
+#Secret: 8oMWRyVQv1fio0lwnFVMMUu3ObY
+
+requests.get('https://cex.io/api/balance/')
+
+APIrequest = API()
